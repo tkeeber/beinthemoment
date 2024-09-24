e3b958a1d8c83773bbc79184e2685a64b4b9cc62
Tom Keeber <tkeeber@dunelm.com>
2024-09-24 12:38:41 +0100

    initial:

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
+import lit from "@astrojs/lit";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://tkeeber.github.io",
+  sitemap: true, // Generate sitemap (set to "false" to disable)
+  integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+});

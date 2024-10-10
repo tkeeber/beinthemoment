5aaf98956499f4217e063df9abf57a675a4513ca
Tom Keeber <tkeeber@dunelm.com>
2024-10-10 16:39:10 +0100

    email changes

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,14 +1,18 @@
 export const nav = [
-	{
-		title: 'Home',
-		slug: '/',
-	},
-	{
-		title: 'About Me',
-		slug: '/company/about',
-	},
-	{
-		title: 'Contact',
-		slug: '/company/contact',
-	},
+  {
+    title: "Home",
+    slug: "/",
+  },
+  {
+    title: "For Business",
+    slug: "/company/for-businesses",
+  },
+  {
+    title: "About Me",
+    slug: "/company/about",
+  },
+  {
+    title: "Contact",
+    slug: "/company/contact",
+  },
 ];
